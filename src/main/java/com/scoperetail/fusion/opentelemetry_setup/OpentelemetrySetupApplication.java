b7a8c29ef1ae0d530f65af8d57300ec0b2cd4c3d
rutuja-botre <103938293+rutuja-botre@users.noreply.github.com>
2022-12-22 16:06:41 +0530

    Add files via upload

---
+++ src/main/java/com/scoperetail/fusion/opentelemetry_setup/OpentelemetrySetupApplication.java
@@ -0,0 +1,13 @@
+package com.scoperetail.fusion.opentelemetry_setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentelemetrySetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentelemetrySetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scoperetail/fusion/opentelemetry_setup/OpentelemetrySetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoperetail.fusion.opentelemetry_setup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentelemetrySetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
