ccf06977540e3d4f8f522bbbcd7eaaef64d71c0c
rutuja-botre <103938293+rutuja-botre@users.noreply.github.com>
2022-12-26 17:44:32 +0530

    Add files via upload

---
+++ src/test/java/com/scoperetail/fusion/fusionconnectobservability/FusionConnectObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoperetail.fusion.fusionconnectobservability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusionConnectObservabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
